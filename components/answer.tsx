e359ed27493c853943384f76c2eb37702baf17d8
Andrew Tarr <atarr@dontpaniclabs.com>
2022-04-05 19:05:21 -0500

    wip

--- components/answer.tsx
+++ components/answer.tsx
@@ -1,28 +1,25 @@
-import { AnswerProp } from './types';
-import { useEffect, useRef, useState } from 'react';
+import { AnswerProp } from "./types";
+import { useEffect, useRef, useState } from "react";
 
 function Answer({ word, wasFound, wasRevealed = false }: AnswerProp) {
     const labelRef = useRef<HTMLLabelElement>(null);
 
     let [wasScrolled, setWasScrolled] = useState(false);
 
-    if (!wasRevealed && wasFound && !wasScrolled) {
-        console.log('1');
-        useEffect(() => {
-            if (labelRef?.current) {
-                labelRef.current.scrollIntoView({
-                    behavior: 'smooth',
-                    inline: 'center',
-                });
+    useEffect(() => {
+        if (!wasRevealed && wasFound && !wasScrolled && labelRef?.current) {
+            labelRef.current.scrollIntoView({
+                behavior: "smooth",
+                inline: "center",
+            });
 
-                setWasScrolled(true);
-            }
-        });
-    }
+            setWasScrolled(true);
+        }
+    });
 
     return (
-        <label className={wasRevealed ? 'revealed' : ''} ref={labelRef}>
-            {wasFound ? word : '-'.repeat(word.length)}
+        <label className={wasRevealed ? "revealed" : ""} ref={labelRef}>
+            {wasFound ? word : "-".repeat(word.length)}
             <style jsx>{`
                 label {
                     margin: 0 8px;
